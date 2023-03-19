ce0fc7c6e0715ff5a182dab73f848c2c19aa4c2d
Tglman <tglman@tglman.com>
2023-03-19 14:11:37 +0000

    feat: add support for links and titles on each specific tile

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use builder_pattern::Builder;
 pub use colorsys::Rgb;
 use quick_xml::events::{BytesDecl, BytesText, Event};
-use quick_xml::Writer;
+use quick_xml::{Error, Writer};
 use std::{io::Write, sync::Mutex};
 
 #[derive(Builder)]
@@ -14,12 +14,14 @@ pub struct Config {
     size_x: u32,
     #[default(15)]
     size_y: u32,
-    #[default(10)]
+    #[default(5)]
     padding_x: u32,
-    #[default(10)]
+    #[default(5)]
     padding_y: u32,
     #[default(1)]
     border: u32,
+    #[default(3)]
+    rounding: u32,
 }
 
 fn coordinate(base: u32, size: u32, padding: u32, offset: u32) -> u32 {
@@ -48,11 +50,19 @@ impl Config {
     fn border(&self) -> String {
         format!("{}", self.border)
     }
+    fn rounding(&self) -> String {
+        format!("{}", self.rounding)
+    }
 }
 
 pub trait Element {
     fn get_color(&self) -> Rgb;
     fn get_border_color(&self) -> Rgb;
+    fn get_link(&self) -> Option<Box<dyn ElementLink>>;
+}
+pub trait ElementLink {
+   fn link(&self) ->String; 
+   fn title(&self) ->String; 
 }
 
 pub fn tile<D, B, E, W>(
@@ -91,24 +101,50 @@ where
                 let mut x = 0;
                 for xval in yval {
                     x += 1;
-                    let style = format!(
-                        "fill:{};stroke-width:{};stroke:{}",
-                        xval.get_color().to_hex_string(),
-                        config.border(),
-                        xval.get_border_color().to_hex_string()
-                    );
-                    svg.create_element("rect")
-                        .with_attributes(vec![
-                            ("x", config.pos_x(x).as_str()),
-                            ("y", config.pos_y(y).as_str()),
-                            ("width", config.size_x().as_str()),
-                            ("height", config.size_y().as_str()),
-                            ("style", style.as_str()),
-                        ])
-                        .write_empty()?;
+                    if let Some(l) = xval.get_link() {
+                        svg.create_element("a")
+                            .with_attributes(vec![
+                                ("xlink:href", l.link().as_str()),
+                                ("xlink:title", l.title().as_str()),
+                            ])
+                            .write_inner_content(|svg|{
+                                write_rect(svg, &config, x, y, &xval)?;
+                                Ok(())
+                            })?;
+                    } else {
+                                write_rect(svg, &config, x, y, &xval)?;
+                    }
                 }
             }
             Ok(())
         })?;
+    Ok(())
+}
+
+fn write_rect<W: std::io::Write>(
+    svg: &mut Writer<W>,
+    config: &Config,
+    x: u32,
+    y: u32,
+    ele: &impl Element,
+) -> std::result::Result<(), Error> {
+    let style = format!(
+        "fill:{};stroke-width:{};stroke:{}",
+        ele.get_color().to_hex_string(),
+        config.border(),
+        ele.get_border_color().to_hex_string()
+    );
+
+    svg.create_element("rect")
+        .with_attributes(vec![
+            ("x", config.pos_x(x).as_str()),
+            ("y", config.pos_y(y).as_str()),
+            ("rx", config.rounding().as_str()),
+            ("ry", config.rounding().as_str()),
+            ("width", config.size_x().as_str()),
+            ("height", config.size_y().as_str()),
+            ("style", style.as_str()),
+        ])
+        .write_empty()?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::OpenOptions;
-use tileline::{tile, Config, Element, Rgb};
+use tileline::{tile, Config, Element, Rgb, ElementLink};
 
 struct Value {
     i: u16,
@@ -16,13 +16,25 @@ impl Element for Value {
     fn get_border_color(&self) -> Rgb {
         Rgb::new(0.0, 0.0, 50.0, None)
     }
+    fn get_link(&self) -> Option<Box<dyn ElementLink>> {
+        Some(Box::new(ElementLinkImpl("https://tglman.com".to_owned(), "Tglman.com".to_owned())))
+    }
+}
+struct ElementLinkImpl(String,String);
+impl ElementLink for ElementLinkImpl {
+    fn link(&self) ->String {
+        self.0.clone()
+    }
+    fn title(&self) ->String {
+        self.1.clone()
+    }
 }
 
 fn main() {
     let mut val = Vec::new();
-    for i in 1..5 {
+    for i in 0..5 {
         let mut column = Vec::new();
-        for z in 1..30 {
+        for z in 0..30 {
             column.push(Value::new(i * 10 + z * 10));
         }
         val.push(column.into_iter());
